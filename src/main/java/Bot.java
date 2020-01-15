9edf6c9e51a163b489954f01c331302d6f01c57f
vbumazhenko <vbumazhenko@mail.ru>
2020-01-15 16:24:46 +0700

    Изменена версия Maven

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -1,12 +1,21 @@
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 public class Bot extends TelegramLongPollingBot {
 
     @Override
     public void onUpdateReceived(Update update) {
 
-        System.out.println("1111111111");
+        SendMessage message = new SendMessage();
+        message.setText("Привет, меня зовут Юрий!");
+        message.setChatId(update.getMessage().getChatId());
+        try {
+            execute(message);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
 
     }
 
